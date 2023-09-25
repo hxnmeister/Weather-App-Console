b3b31d8f52270c7d1e96ca277c4f91a5d5d3418e
Valerii <valeriy.stronskiy@gmail.com>
2023-09-25 10:49:27 +0300

    Added a class for connecting and displaying the weather. In main I added a check when entering the city name and time.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 namespace Weather_App
 {
@@ -10,6 +13,49 @@ namespace Weather_App
     {
         static void Main(string[] args)
         {
+            string cityName = null;
+            DateTime? timeDay = null;
+
+            do
+            {
+                Console.WriteLine("\n  To find out the weather, enter the city and time of day (leave the field blank to get an hourly forecast)");
+
+                do
+                {
+                    Console.Write("\n Enter city name: ");
+                    cityName = Console.ReadLine();
+
+                    Console.Clear();
+                    if (!(new Regex("^[a-zA-Z]{2,}$").Match(cityName).Success)) Console.WriteLine($"\n City name \"{cityName}\" is incorrect, try again!");
+                    else break;
+
+                } while (true);
+
+                do
+                {
+                    Console.Write("\n Enter time(HH:mm): ");
+                    string tempTime = Console.ReadLine();
+
+                    Console.Clear();
+                    if (string.IsNullOrWhiteSpace(tempTime))
+                    {
+                        break;
+                    }
+                    else if (DateTime.TryParse(tempTime, out DateTime result))
+                    {
+                        timeDay = result;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n Entered format \"{tempTime}\" is incorrect, try again!");
+                    }
+
+                } while (true);
+
+                new Weather().ShowWeather("Sumy", timeDay?.ToString("HH:mm"));
+
+            } while (true);
         }
     }
 }
---
+++ Weather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Weather_App
+{
+    internal class Weather
+    {
+        private readonly string API_KEY;
+        private readonly string API_URL;
+
+        public Weather(string API_URL = "https://api.weatherapi.com/v1/forecast.xml", string API_KEY = "ca4b2195d9d947638a965036233107") 
+        { 
+            this.API_KEY = API_KEY;
+            this.API_URL = API_URL;
+        }
+
+        public void ShowWeather(string cityName, string time)
+        {
+            try
+            {
+                string webEndPoint = $"{API_URL}?key={API_KEY}&q={cityName}&days=2";
+                string tommorow = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd");
+
+                WebClient receiver = new WebClient();
+                XDocument weatherData = XDocument.Parse(Encoding.UTF8.GetString(receiver.DownloadData(webEndPoint)));
+                XElement root = weatherData.Root;
+                XElement forecastDay = root.Element("forecast").Elements("forecastday").FirstOrDefault(x => x.Element("date").Value == tommorow);
+
+
+            }
+            catch(WebException we)
+            {
+                Console.WriteLine("\n An exception was thrown while retrieving information from the site!");
+                Console.WriteLine($" Exception Message: {we.Message}\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n An exception occurred during execution!");
+                Console.WriteLine($" Exception Message: {ex.Message}\n");
+            }
+        }
+    }
+}
